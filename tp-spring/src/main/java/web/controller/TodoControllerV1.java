c53b788d981460648eb2c7aa3f2817f4af9e59c9
vbarat <vincent.barat@gmail.com>
2023-04-05 10:54:41 +0200

    TP2  - Q2.1 POST v2  - Q2.2 DELETE

--- tp-spring/src/main/java/web/controller/TodoControllerV1.java
+++ tp-spring/src/main/java/web/controller/TodoControllerV1.java
@@ -1,16 +1,23 @@
 package web.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import web.model.Category;
 import web.model.Todo;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/public/todo")
 @CrossOrigin
 public class TodoControllerV1 {
+
+    private int cpt;
+    private Map<Integer, Todo> todos = new HashMap<>();
 
     public TodoControllerV1() {
     }
@@ -25,8 +32,19 @@ public class TodoControllerV1 {
         return new Todo(1, "A title", "desc", List.of(Category.ENTERTAINMENT, Category.WORK), null, "foo");
     }
 
-    @PostMapping(path = "todo")
-    public void post(@RequestBody Todo todo) {
-        System.out.println(todo);
+    @PostMapping(path = "todo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Todo post(@RequestBody Todo todo) {
+        todo.setId(cpt);
+        todos.put(cpt, todo);
+        cpt++;
+        System.out.println(todos);
+        return todo;
+    }
+
+    @DeleteMapping(path = "todo/{id}")
+    public void deleteTodo(@PathVariable("id") Integer id) {
+        if (todos.get(id) == null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        todos.remove(id);
     }
 }
