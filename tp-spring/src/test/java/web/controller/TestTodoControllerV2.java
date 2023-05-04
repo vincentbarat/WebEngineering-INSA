041ad27276c3ef408d25fb44b4a0287f76a1abc0
vbarat <vincent.barat@gmail.com>
2023-05-04 17:55:20 +0200

    TP6: Fix tests

--- tp-spring/src/test/java/web/controller/TestTodoControllerV2.java
+++ tp-spring/src/test/java/web/controller/TestTodoControllerV2.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -16,8 +15,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class TestTodoControllerV2 {
+
     @Autowired
     private MockMvc mvc;
 
--- tp-spring/src/test/java/web/service/TestTodoService.java
+++ tp-spring/src/test/java/web/service/TestTodoService.java
@@ -1,10 +1,5 @@
 package web.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -12,14 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import web.model.Category;
 import web.model.Todo;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestTodoService {
+
     @MockBean
+    //@Autowired
     private TodoCrudRepository repository;
 
     @Autowired
@@ -28,7 +28,7 @@ public class TestTodoService {
     @Test()
     public void saveCalledWhenAddingATodo() {
         Todo todo = new Todo(0L, "title 1", "bar", List.of(Category.LOW_PRIORITY), null, "foo");
-        Todo todo2 = new Todo(2L, "title 2", "bar", List.of(Category.LOW_PRIORITY), null, "foo");
+        Todo todo2 = new Todo(1L, "title 1", "bar", List.of(Category.LOW_PRIORITY), null, "foo");
 
         // Configuring the mock so that a call to 'save' with 'todo' will return the same todo
         Mockito.when(repository.save(todo)).thenReturn(todo2);
@@ -37,7 +37,8 @@ public class TestTodoService {
 
         // Checking that the save method has been called one time with 'todo'
         Mockito.verify(repository, Mockito.times(1)).save(todo);
+
         // Checking that the method returns the result of 'save'.
-        assertSame(todo2, res);
+        assertEquals(todo2, res);
     }
 }
